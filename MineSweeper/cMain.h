227751b680ea9db96a02f2af9e4849482ba6e2df
LaTrae' Swann <104823604+Latrae@users.noreply.github.com>
2022-06-09 03:34:31 -0400

    Add files via upload

---
+++ MineSweeper/cMain.h
@@ -0,0 +1,26 @@
+#pragma once
+//#include "../Users/non original orgasm/Desktop/sdks/wxWidgets-3.1.6/include/wx/wx.h"
+#include "wx/wx.h"
+
+
+
+
+class cMain : public wxFrame
+{
+
+public:
+	cMain();
+	~cMain();
+	void OnButtonClicked(wxCommandEvent& evt);
+
+	wxDECLARE_EVENT_TABLE();
+
+public:
+	int FieldWidth = 10;
+	int FieldHeight = 10;
+	wxButton** btn;
+	int* nField = nullptr;
+	bool bFirstClick = true;
+
+};
+
